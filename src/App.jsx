6c3a70a5932fd45aab1592ef9708b69c05363598
Hemant <68590578+Hemant-29@users.noreply.github.com>
2024-08-17 23:53:12 +0530

    frontend changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,47 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 
+const url = "http://127.0.0.1:8000/api/courses/";
+
 function App() {
-  const [count, setCount] = useState(0);
+  const [courses_data, set_courses_data] = useState([]);
+
+  useEffect(() => {
+    fetch(url)
+      .then((res) => {
+        if (res.ok) {
+          console.log("response ok!");
+          return res.json();
+        } else {
+          throw new Error("response was not ok.");
+        }
+      })
+      .then((data) => {
+        const fetched_data = data;
+        set_courses_data(fetched_data);
+        console.log("fetched data:", fetched_data);
+      });
+  }, []);
+  // Empty dependency array
+
+  // Log courses_data in useEffect to see when it updates
+  useEffect(() => {
+    console.log("Courses data:", courses_data);
+  }, [courses_data]);
 
   return (
     <>
       <h1>Hello World from React!</h1>
+      <h2>Accessing API from backend:</h2>
+      <div>
+        {courses_data.map((element, index) => (
+          <ul key={index}>
+            <li>{element.title || "No title available"}</li>
+            <li>{element.course_code || "No course code available"}</li>
+            <li>{element.description || "No description available"}</li>
+          </ul>
+        ))}
+      </div>
     </>
   );
 }
